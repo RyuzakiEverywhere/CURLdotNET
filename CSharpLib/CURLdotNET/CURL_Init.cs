0d5e8c351c608a2a98d83872658eb327d548e1b4
RyuzakiEverywhere aka LLawlietEverywhere <119974711+RyuzakiEverywhere@users.noreply.github.com>
2022-12-12 13:29:11 +0200

    Add files via upload

---
+++ CSharpLib/CURLdotNET/CURL_Init.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CURLdotNET
+{
+    public class CURLInit
+    {
+        [DllImport(@"curl_dll_lib.dll")]
+        public static extern bool InitCURL(string hostname);
+        public bool CURL_Init(string hostname_ip)
+        {
+            if(InitCURL(hostname_ip))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ CSharpLib/CURLdotNET/TORDownloaded.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CURLdotNET
+{
+    public class TORDownloaded
+    {
+
+        [DllImport(@"curl_dll_lib.dll")]
+        public static extern bool CheckTor(string filename_phpfile);
+        public bool TOR_Downloaded(string php_name)
+        {
+            if(CheckTor(php_name))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
